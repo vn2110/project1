d9a02f1d90f81c27b480fd9fd493aedbdadca74c
Thanh Dinh Van <acc.thanhdv.000@gmail.com>
2017-02-28 10:25:37 +0000

    edit hello.hava

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ import package vn.thanhdv.javatest;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Hello Javaa");
     }
 
 }
